eb46259fa08078d1deeb0bd30776c18a61f65687
Rob Tayler <trober498@gmail.com>
2024-01-26 20:17:09 +0000

    Simple Move and Jump

--- GGJ/Assets/PlayerMovement.cs
+++ GGJ/Assets/PlayerMovement.cs
@@ -5,38 +5,61 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    bool CanJump = true;
 
     [SerializeField]
     private float WalkSpeed = 2.0f;
 
-    Rigidbody2D thisBody;
+    Rigidbody2D ThisBody;
+
+    float distToGround = 0.0f;
 
 
     void Start()
     {
-        thisBody = GetComponent<Rigidbody2D>();
+       
+        distToGround =  GetComponent<BoxCollider2D>().bounds.extents.y;
+        ThisBody = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0,0);
-        transform.Translate(movement* WalkSpeed * Time.deltaTime);
+        transform.Translate(movement * WalkSpeed * Time.deltaTime);
 
-        Debug.DrawRay(transform.position, -Vector3.up * 4);
+        Debug.DrawRay(transform.position, -Vector3.up* (distToGround ), Color.blue);
+
+        if (Grounded() && Input.GetButton("Jump") && CanJump)
+        {
+            Jump();
+        }
+
     }
 
-    bool Grounded()
+    public bool Grounded()
     {
-        return true;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, (distToGround ),LayerMask.GetMask("Level"));
 
-        
-
+        if (hit.collider)
+        {
+            if(!CanJump && ThisBody.velocity.y<0)
+            {
+                CanJump = true;
+            }
+            return true;
+           
+        }
+        else
+        {
+            return false;
+        }
     }
 
 
     void Jump()
     {
-        
+        ThisBody.AddForce(Vector3.up * 10, ForceMode2D.Impulse);
+        CanJump = false;
     }
 }
