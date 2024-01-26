a355b923182e8075b44c9f23b025929f168878f9
Rob Tayler <trober498@gmail.com>
2024-01-26 19:14:33 +0000

    Character Object Created

---
+++ GGJ/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float WalkSpeed = 2.0f;
+
+    Rigidbody2D thisBody;
+
+
+    void Start()
+    {
+        thisBody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0,0);
+        transform.Translate(movement* WalkSpeed * Time.deltaTime);
+
+        
+    }
+
+    bool Grounded()
+    {
+        return true;
+
+    }
+
+
+    void Jump()
+    {
+        
+    }
+}
